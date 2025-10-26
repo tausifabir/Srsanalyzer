9d94cc87bdf18691c883a09102df06730f2848d6
tausif.islam <tausif.islam@bracits.com>
2025-10-26 16:24:26 +0600

    Initial Commit

---
+++ src/main/java/com/example/srsanalyzer/SrsanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.srsanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrsanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrsanalyzerApplication.class, args);
+	}
+
+}
